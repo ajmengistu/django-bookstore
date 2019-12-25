c5d14fe4c064b6406855de05bd5efbfa6cf98aac
ajmengistu <a_mengistu@outlook.com>
2019-12-25 10:04:28 -0500

    Connected this app to a Postgres db running on Docker

--- app/bookstore/bookstore/settings.py
+++ app/bookstore/bookstore/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bookstore_dev',                      
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
